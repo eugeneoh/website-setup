95da09900a52c68593436228cab1bd8280ed8c1c
Eugene Oh <eugene@visor.gg>
2020-07-13 17:17:59 -0700

    Updates

---
+++ precache-manifest.61f4083e2c9de687e0e4f115bedaab84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3110e73e5d8615121cf45ebab5ece86d",
+    "url": "/website-setup/index.html"
+  },
+  {
+    "revision": "c816778166c9e4d4ec6c",
+    "url": "/website-setup/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa2ba2303b37f4f392b8",
+    "url": "/website-setup/static/js/2.063c2248.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website-setup/static/js/2.063c2248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c816778166c9e4d4ec6c",
+    "url": "/website-setup/static/js/main.6d63dbfb.chunk.js"
+  },
+  {
+    "revision": "868eded7d761d3c9bde3",
+    "url": "/website-setup/static/js/runtime-main.28a77243.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website-setup/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6243f198913ddce4277d81dc700344c.js"
+  "/website-setup/precache-manifest.61f4083e2c9de687e0e4f115bedaab84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website-setup/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

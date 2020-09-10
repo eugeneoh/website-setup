bd198aa7005efe50e5f937a3a9ef090727d3d019
Eugene Oh <eugene@visor.gg>
2020-09-10 15:49:14 -0700

    style leftnav

--- src/components/LeftNav/index.js
+++ src/components/LeftNav/index.js
@@ -4,7 +4,36 @@ import "./styles.css";
 const LeftNav = () => {
   return (
     <div className="LeftNav">
-      HI
+      <div className="AboutMe">
+        <div className="header">
+          NAME
+        </div>
+        <div className="subheader">
+          Laura Zhang
+        </div>
+        <div className="spacer"/>
+        <div className="header">
+          ROLE
+        </div>
+        <div className="subheader">
+          Digital Marketer
+        </div>
+        <div className="spacer"/>
+        <div className="header">
+          EMAIL
+        </div>
+        <div className="subheader">
+          laurazee@gmail.com
+        </div>
+        <div className="spacer"/>
+        <div className="header">
+          PHONE
+        </div>
+        <div className="subheader">
+          1-800-HIRE-ME
+        </div>
+
+      </div>
     </div>
   );
 }
